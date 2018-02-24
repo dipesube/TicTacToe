e4d36eb2b1ab1e106b665a244d990556a7a1f111
Dipendra Subedi <33377586+dipesube@users.noreply.github.com>
2018-02-24 10:37:34 -0500

    Add files via upload

---
+++ Gamee.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+
+
+
+/**
+ *
+ * @author Geust
+ */
+public class Gamee {
+    
+    char[][] board = new char[3][3];
+    boolean choice = true;
+    int scorex = 0;
+    int scoreo = 0;
+    int mode;
+    String turn = "X";
+    int draw = 0;
+    boolean winner = false;
+    boolean Draw = false;
+    public Gamee() { //constructor
+        
+
+    }
+    
+}
